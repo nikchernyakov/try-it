d1900ba196bb4bccaf592ccce5a11fafcb6d2468
nchernia <Nikita.Cherniakov@t-systems.ru>
2019-07-03 16:21:51 +0300

    Resolve task 2 of angular-udemy-course

--- Web/Front/try-angular/basics-assignment-2-start/src/app/app.component.ts
+++ Web/Front/try-angular/basics-assignment-2-start/src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    username = '';
+
+    public OnResetButtonClicked() {
+        this.username = '';
+    }
 }
